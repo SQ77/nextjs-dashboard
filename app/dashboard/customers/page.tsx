198155569def56b6b5b06705570e819758e1e5c3
SQ77 <andrewzsq@gmail.com>
2025-02-14 19:23:13 +0800

    Add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
 import Table from "@/app/ui/customers/table";
 import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Customers",
+};
 
 export default async function Page(props: {
     searchParams?: Promise<{
@@ -11,7 +16,5 @@ export default async function Page(props: {
     const query = searchParams?.query || "";
     const customers = await fetchFilteredCustomers(query);
 
-    return (
-        <Table customers={customers}/>
-    );
+    return <Table customers={customers} />;
 }
